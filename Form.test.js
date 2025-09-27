f1cf9ec9eaf42274d1511eccfc639fc59c5f7041
Marcus Warnert <mrvwarnert@gmail.com>
2025-09-27 16:13:11 -0400

    k

---
+++ Form.test.js
@@ -0,0 +1,23 @@
+//○ Full Name (Text Input)
+
+
+//○ Email Address (Email Input)
+
+
+//○ Password (Password Input for account verification)
+
+
+//○ Feedback Category (Dropdown/Select Input)
+
+
+//○ Preferred Contact Method (Radio Buttons)
+
+
+//○ Additional Features of Interest (Checkboxes)
+
+
+//○ Comments (Textarea)
+
+
+//○ Submit Button (to send the data)
+
